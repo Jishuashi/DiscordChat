597a546ba4bfce5ecb40e98eea44f447d7a9c0f2
Hugo Chartier <chartierhugo098@gmail.com>
2025-06-02 14:59:05 +0200

    Init Plugins project

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,19 @@ group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
 }
 
 dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
     testImplementation(kotlin("test"))
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 tasks.test {
