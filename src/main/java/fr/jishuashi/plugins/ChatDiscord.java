dd52089f958f868bd448cb4841a6b1bc27c213ac
Hugo Chartier <chartierhugo098@gmail.com>
2025-06-06 15:16:49 +0200

    Discord to plugins

--- src/main/java/fr/jishuashi/plugins/ChatDiscord.java
+++ src/main/java/fr/jishuashi/plugins/ChatDiscord.java
@@ -11,9 +11,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.io.OutputStream;
+
+import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
 import java.net.URL;
+import java.util.stream.Collectors;
 
 import com.google.gson.JsonObject;
 import net.luckperms.api.LuckPerms;
@@ -22,7 +25,14 @@ import net.luckperms.api.model.user.User;
 import net.luckperms.api.query.QueryOptions;
 import org.bukkit.entity.Player;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 public class ChatDiscord extends JavaPlugin implements Listener {
+
+    private ChatDiscord instance;
+
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
@@ -32,6 +42,49 @@ public class ChatDiscord extends JavaPlugin implements Listener {
 
         String serverId = getConfig().getString("server-id");
         getLogger().info("Plugin activé pour : " + serverId);
+
+        this.instance = this;
+        startHttpServer();
+    }
+
+    private void startHttpServer() {
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(4001), 0);
+            server.createContext("/mc-endpoint", new HttpHandler() {
+                @Override
+                public void handle(HttpExchange exchange) throws IOException {
+                    if (!exchange.getRequestMethod().equalsIgnoreCase("POST")) {
+                        exchange.sendResponseHeaders(405, -1);
+                        return;
+                    }
+
+                    InputStream inputStream = exchange.getRequestBody();
+                    String body = new BufferedReader(new InputStreamReader(inputStream))
+                            .lines().collect(Collectors.joining("\n"));
+                    inputStream.close();
+
+                    JsonObject json = new Gson().fromJson(body, JsonObject.class);
+                    String player = json.has("name") ? json.get("name").getAsString() : "???";
+                    String message = json.has("message") ? json.get("message").getAsString() : "message";
+
+                    // Envoie dans le chat principal
+                    Bukkit.getScheduler().runTask(instance, () -> {
+                        Bukkit.broadcastMessage("§9[Discord] §r" + player + "§f: " + message);
+                    });
+
+                    String response = "Reçu";
+                    exchange.sendResponseHeaders(200, response.length());
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();
+                }
+            });
+            server.setExecutor(null);
+            server.start();
+            getLogger().info("HTTP server started on port 4001");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @EventHandler
